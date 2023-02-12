5e3d87549b83c4c7c4d8af446d30a280d661f739
grasshopper42 <solite4686@gmail.com>
2023-02-13 01:08:00 +0900

    feat: add share count feature

--- lib/live_view_counter_web/live/counter.ex
+++ lib/live_view_counter_web/live/counter.ex
@@ -1,16 +1,27 @@
 defmodule LiveViewCounterWeb.Counter do
   use LiveViewCounterWeb, :live_view
 
+  @topic "live"
+
   def mount(_params, _session, socket) do
+    LiveViewCounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :count, 0)}
   end
 
   def handle_event("increment", _params, socket) do
-    {:noreply, update(socket, :count, &(&1 + 1))}
+    new_state = update(socket, :count, &(&1 + 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "increment", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("decrement", _params, socket) do
-    {:noreply, update(socket, :count, &(&1 - 1))}
+    new_state = update(socket, :count, &(&1 - 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "decrement", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, count: msg.payload.count)}
   end
 
   def render(assigns) do
